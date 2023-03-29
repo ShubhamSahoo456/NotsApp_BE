c7d56989e036b5c496b58177533cdfe8d5f2a7f2
shubhamsahookiwi <shubham.sahoo@kiwitech.com>
2023-03-29 15:43:17 +0530

    get api

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
 app.use("/api/v1", otproutes);
 app.use("/api/v1", userRoutes);
 expressWs.app.ws("/echo/:id", (ws, req) => {
